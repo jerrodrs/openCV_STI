bfc898bb80569f1132224a3a30339ec3128eeb03
Jerrod Seger <jseger@sfu.ca>
2016-11-15 17:06:12 -0800

    horizontal and vertical STI rendering

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
+	/*
     char filename[1024];
     printf("Enter a filename (e.g. small.ogv): ");
     scanf("%s", filename);
@@ -15,8 +16,8 @@ int main(int argc, char* argv[])
         fprintf(stderr, "Please select a file\n");
         return EXIT_FAILURE;
     }
-    
-    VideoCapture vc = VideoCapture(filename);
+    */
+    VideoCapture vc = VideoCapture("small_test.mp4");
     int frameCount = vc.get(CV_CAP_PROP_FRAME_COUNT);
     int frameHeight = vc.get(CV_CAP_PROP_FRAME_HEIGHT );
     int frameWidth = vc.get(CV_CAP_PROP_FRAME_WIDTH );
@@ -25,13 +26,24 @@ int main(int argc, char* argv[])
     Mat image;
 
     //constructed STI frame for center columns
-    Mat STI_image_cols(frameWidth, frameCount, CV_8UC3, Scalar::all(0));
+    Mat STI_image_cols(frameHeight, frameCount, CV_8UC3, Scalar::all(0));
 
     //constructed STI frame for center rows
-    Mat STI_image_rows(frameHeight, frameCount, CV_8UC3, Scalar::all(0));
+    Mat STI_image_rows(frameWidth, frameCount, CV_8UC3, Scalar::all(0));
 
     // Create a window
-    namedWindow("Display", WINDOW_NORMAL);
+    namedWindow("PLAYER", WINDOW_NORMAL);
+    namedWindow("STI_COL_DISPLAY", WINDOW_NORMAL);
+    namedWindow("STI_ROW_DISPLAY", WINDOW_NORMAL);
+
+    moveWindow("PLAYER", 50, 50);
+    moveWindow("STI_COL_DISPLAY", 50+frameWidth, 50);
+    moveWindow("STI_ROW_DISPLAY", 50+frameWidth+frameWidth, 50);
+
+    int centerColumn = frameWidth/2;
+    int centerRow = frameHeight/2;
+
+    int currentColumn = 0;
 
     while (true)
     {
@@ -41,21 +53,26 @@ int main(int argc, char* argv[])
         // Stop if there are no more frames in the video
         if (image.empty()) break;
 
-        // Draw some colours on the image
-        for (int col = 0; col < image.size().width; col++)
-        for (int row = 16; row < 32; row++) {
-            Vec3b bgrPixel = image.at<Vec3b>(row, col);
-            if (col < image.size().width / 3)
-                bgrPixel[0] = 255; // left third, blue
-            else if (col < image.size().width * 2 / 3)
-                bgrPixel[1] = 255; // middle third, green
-            else
-                bgrPixel[2] = 255; // right third, red
-            image.at<Vec3b>(row, col) = bgrPixel;
-        }
+        if(currentColumn <= frameCount){
+	        // Transfer center column to new image
+	        for (int row = 0; row < image.size().height; row++) {
+	            Vec3b bgrPixel = image.at<Vec3b>(row, centerColumn);
+	            STI_image_cols.at<Vec3b>(row, currentColumn) = bgrPixel;
+	        }
+	        // Transfer center row to new image
+	        for (int col = 0; col < image.size().width; col++) {
+	            Vec3b bgrPixel = image.at<Vec3b>(centerRow, col);
+	            STI_image_rows.at<Vec3b>(col, currentColumn) = bgrPixel;
+	        }
+	    }
+
+	    currentColumn++;
+	    
 
         // Display the image 
-        imshow("Display", image);
+        imshow("PLAYER", image);
+        imshow("STI_COL_DISPLAY", STI_image_cols);
+        imshow("STI_ROW_DISPLAY", STI_image_rows);
 
         // Sleep for 30 milliseconds. Note: This function is the only method in 
         // HighGUI that can fetch and handle events,  so it needs to be called 
